6da97502597a4635ca9daa9f36ac23d8eacbfcd6
mrmusicman <110935076+mrmusicman@users.noreply.github.com>
2022-08-09 15:20:39 -0400

    Create spotifyface.py

---
+++ spotifyface.py
@@ -0,0 +1,11 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from config import * #put in cid and secret as two variables in a file called config.py with your API keys
+
+
+#Authentication - without user
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+def apiTrackToNormal(dc):
+    return [dc['id'], dc['name'], dc['artists'][0]['name'], dc['album']['images'][0]['url']]
