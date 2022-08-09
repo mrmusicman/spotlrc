9f8cedd36a717d902f8c95c573a1da403d4bf4a6
mrmusicman <110935076+mrmusicman@users.noreply.github.com>
2022-08-09 15:19:56 -0400

    Create miscu.py

---
+++ miscu.py
@@ -0,0 +1,10 @@
+import unicodedata
+import re
+def slugify(value, allow_unicode=False):
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
